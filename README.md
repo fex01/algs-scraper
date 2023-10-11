9042b1fb97484e5e55a25c9615404191a20c1cd8
fex <git@fexmail.de>
2023-10-11 11:35:07 +0200

    publish

---
+++ README.md
@@ -0,0 +1,65 @@
+# ALGS News Scraper
+
+## Intent
+
+This project aims to scrape the website of the [Annedore-Leber-Grundschule](https://algs.de/) for news updates, focusing on two sections: `Veranstaltungen` and `Aktuelles`.
+
+## Necessity
+
+The ALGS website does not offer RSS feeds or any other method of push notifications to keep interested parties updated on news and events. This scraper addresses this limitation by offering a way to get regular updates.
+
+## Intended Use
+
+The primary use case for this scraper is not to self-host it but to join the ALGS News group on Telegram, where you'll receive push notifications for updates in the `Veranstaltungen` and `Aktuelles` sections. To join the group and start receiving updates, please contact me for access.
+
+### Note: Quick & Dirty Solution
+
+This scraper is a quick and dirty solution, developed in with and by ChatGPT. It's intended to serve a specific need and might not be the most elegant or efficient codebase.
+
+## Self-Hosting (Optional)
+
+If you wish to adapt the project to your specific requirements or contribute, you can self-host the scraper.
+
+### Pre-requisites
+
+- Python 3.x
+- pip3
+
+### Installation Steps
+
+1. **Clone the Repository:**
+    ```bash
+    git clone <repository_url>
+    ```
+
+2. **Navigate to the Project Folder:**
+    ```bash
+    cd <project_folder>
+    ```
+
+3. **Adapt .env File:**
+    - Copy the `.env_template` file and rename it to `.env`.
+    - Fill in your Telegram bot credentials.
+    - **Note:** Keep this file on your local machine; it should not be committed to the git repository.
+
+    ```bash
+    cp .env_template .env
+    vim .env  # Use your preferred text editor to add credentials.
+    ```
+
+4. **Install Requirements:**
+    ```bash
+    pip3 install -r requirements.txt
+    ```
+
+5. **Set Up a Cron Job:**
+    - To ensure the script runs at regular intervals, you can set up a cron job:
+    ```bash
+    crontab -e  # This will open the cron table for editing in the default editor.
+    ```
+    - Add a new line to execute the script at your desired frequency, for example, every hour:
+    ```bash
+    0 * * * * /usr/bin/python3 /path/to/your/script.py
+    ```
+
+Congratulations, you've successfully set up the option for self-hosting the ALGS News Scraper. If you've opted for this, you should now start receiving updates via Telegram for the `Veranstaltungen` and `Aktuelles` sections of the ALGS website.
---
+++ main.py
@@ -0,0 +1,106 @@
+import requests
+from bs4 import BeautifulSoup
+import hashlib
+from dotenv import load_dotenv
+import os
+import re
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Read bot values from environment variables
+TELEGRAM_BOT_NAMES = os.getenv("TELEGRAM_BOT_NAMES").split(",")
+TELEGRAM_TOKENS = os.getenv("TELEGRAM_TOKENS").split(",")
+CHAT_IDS = os.getenv("CHAT_IDS").split(",")
+
+def fetch_website_content(url):
+    response = requests.get(url)
+    return response.text
+
+def extract_relevant_section(html, tag, attrs):
+    soup = BeautifulSoup(html, 'html.parser')
+    section = soup.find(tag, attrs)
+    return str(section)
+
+def compute_hash(text):
+    return hashlib.sha256(text.encode()).hexdigest()
+
+def convert_to_welcome_text(section):
+    soup = BeautifulSoup(section, 'html.parser')
+    entries = soup.find_all('span')
+    text_output = "Veranstaltungen\n"
+    date = ""
+    for entry in entries:
+        if 'class' in entry.attrs:
+            if 'xr_s9' in entry['class']:
+                date = entry.text
+            elif 'xr_s10' in entry['class']:
+                text_output += f"- {date}: {entry.text}\n"
+    return text_output
+
+
+def convert_to_news_text(section):
+    soup = BeautifulSoup(section, 'html.parser')
+    contentbox_div = soup.find('div', {'id': 'contentbox'})
+    text_output = ""
+
+    if contentbox_div:
+        first_ul = contentbox_div.find('ul')
+        if first_ul:
+            first_li = first_ul.find('li')
+            if first_li:
+                text_output += re.sub('<[^<]+?>', '', str(first_li))
+    return text_output.strip()
+
+def send_telegram_notification(new_table_text):
+    for bot_name, TELEGRAM_TOKEN, CHAT_ID in zip(TELEGRAM_BOT_NAMES, TELEGRAM_TOKENS, CHAT_IDS):
+        send_text = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage?chat_id={CHAT_ID}&text={new_table_text}"
+        response = requests.get(send_text)
+
+        if response.status_code != 200:
+            print(f"Failed to send Telegram notification via {bot_name}: {response.content}")
+        else:
+            print(f"Successfully sent Telegram notification via {bot_name}.")
+
+# Initialize previous hash
+prev_hashes = {'welcome': None, 'news': None}
+
+try:
+    with open('prev_hashes.txt', 'r') as f:
+        lines = f.readlines()
+        prev_hashes['welcome'] = lines[0].strip()
+        prev_hashes['news'] = lines[1].strip()
+except FileNotFoundError:
+    pass
+
+# Check both URLs
+urls_sections = [
+    ('https://algs.de/willkommen.php', 'div', {'id': 'veranstaltungen'}),
+    ('https://algs.de/aktuelles.php', 'div', {'id': 'contentbox'})
+]
+
+for url, tag, attrs in urls_sections:
+    html = fetch_website_content(url)
+    section_html = extract_relevant_section(html, tag, attrs)
+    current_hash = compute_hash(section_html)
+
+    section_key = 'welcome' if 'willkommen' in url else 'news'
+
+    if current_hash != prev_hashes[section_key]:
+        if section_key == 'welcome':
+            text_output = convert_to_welcome_text(section_html)
+        elif section_key == 'news':
+            text_output = convert_to_news_text(section_html)
+        else:
+            text_output = "Updated content found!"
+
+        send_telegram_notification(text_output)
+        print(f'{url} updated')
+
+        # Update the stored hash
+        prev_hashes[section_key] = current_hash
+
+# Save the updated hashes
+with open('prev_hashes.txt', 'w') as f:
+    f.write(f"{prev_hashes['welcome']}\n")
+    f.write(f"{prev_hashes['news']}\n")
